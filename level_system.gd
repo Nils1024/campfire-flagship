75efbd8879d0b00d111e2d043e945d1f72c06ad4
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-02-20 22:34:54 -0800

    fixed level system

---
+++ level_system.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var spawn_queue = []
+@onready var tower_scene = preload("res://tower.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not spawn_queue.is_empty():
+		var enemy: PathFollow2D = spawn_queue.pop_front()
+		var animation_player: AnimationPlayer = enemy.get_child(2)
+		$Path2D.add_child(enemy)
+		animation_player.play("Walk")
+
+func add():
+	var new_tower = tower_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+	spawn_queue.append(new_tower)
+	print
