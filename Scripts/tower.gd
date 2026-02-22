1954cfe753c84ac803c96e4cb88acb54fc9936a8
JoshIsYes <joshuatw423@gmail.com>
2026-02-22 11:58:21 -0800

    The End

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -7,18 +7,17 @@ var can_attack := true
 
 func _process(_delta):
 	$Area.scale = Vector2(Shop.towerRadius, Shop.towerRadius)
-	if can_attack and children.size() > 0:
-		attack()
 
 
 func attack():
+	print("Tower attacked")
 	can_attack = false
 	
-	remove_children()
-	
-	
+	# Wait BEFORE shooting (prevents instant delete feeling)
 	var final_reload = base_reload_time * Shop.reloadtime
 	await get_tree().create_timer(final_reload).timeout
+	
+	remove_children()
 	
 	can_attack = true
 
@@ -26,7 +25,8 @@ func attack():
 func _on_area_area_entered(area: Area2D) -> void:
 	var enemy = area.get_parent()
 	if enemy.is_in_group("Enemy"):
-		children.append(enemy)
+		if not children.has(enemy):
+			children.append(enemy)
 
 
 func _on_area_area_exited(area: Area2D) -> void:
@@ -39,6 +39,13 @@ func remove_children():
 	var children_to_erase = []
 	
 	for child in children:
+		
+		if not is_instance_valid(child):
+			continue
+		
+		if not child.has_meta("HEALTH"):
+			continue
+		
 		var health = child.get_meta("HEALTH")
 		
 		if health <= 1:
@@ -52,7 +59,6 @@ func remove_children():
 			children_to_erase.append(child)
 			child.queue_free()
 		else:
-			
 			child.set_meta("HEALTH", health - Shop.strenght)
 	
 	for child in children_to_erase:
